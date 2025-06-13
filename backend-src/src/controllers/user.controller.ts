f0f5d76d31610525844dc0853c713c8fcba975d7
akashpg-M <waybooster.pg@gmail.com>
2025-06-13 23:32:08 +0530

    backend files

---
+++ backend-src/src/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "../config/http.config";
+import { getCurrentUserService } from "../services/user.service";
+
+export const getCurrentUserController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?.id;
+    if(!userId){
+      throw new Error("User not authorized");
+    }
+    
+    const { user } = await getCurrentUserService(userId);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "User fetch successfully",
+      user,
+    });
+  }
+);
