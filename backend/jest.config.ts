3ceb8b0f3fe4cb70e7c815119407cdfcc55ec47c
akashpg-M <waybooster.pg@gmail.com>
2025-06-23 20:35:55 +0530

    project completed

--- backend/jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'], // Where your test files will live
-  setupFilesAfterEnv: ['./test/jest.setup.ts'], // Optional for extra setup
-  clearMocks: true,
-};
